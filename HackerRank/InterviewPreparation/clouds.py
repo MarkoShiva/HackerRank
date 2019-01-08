7a609abad1fb41c50034a57f24231a9c6e947beb
Marko in1t3r Pavlovic <marko.shiva.pavlovic@gmail.com>
2019-01-08 23:41:41 +0100

    Comments and commit
    Signed-off-by: Marko in1t3r Pavlovic <marko.shiva.pavlovic@gmail.com>

--- HackerRank/InterviewPreparation/clouds.py
+++ HackerRank/InterviewPreparation/clouds.py
@@ -1,3 +1,6 @@
+# You are given the integer N and array with 1 and 0 you should find the smalest amount of hops from starting 0 to last 0
+# considered that can move 2 or 1 places in a row and cannot get into position of num 1.
+
 n = int(input())
 arr = "".join(input().split(" "))
 def clouds(arr):
@@ -7,13 +10,13 @@ def clouds(arr):
         try:
             if arr[2] == "0":
                 hops += 1
-                arr = arr[2:]
+                arr = arr[2:]           # pretty much the same as previous only here done with a string slicing
             elif arr[1] == "0":
                 hops += 1
                 arr = arr[1:]
         except IndexError:
             if len(arr) > 1:
-                if arr[1] == "0":
+                if arr[1] == "0":       # We need to check here again for arr[1]
                     hops += 1
                     arr = arr[1:]
                     continue
--- HackerRank/InterviewPreparation/hacker.py
+++ HackerRank/InterviewPreparation/hacker.py
@@ -7,19 +7,19 @@ import re
 import sys
 
 # Complete the sockMerchant function below.
-def sockMerchant(n, ar):
+def sockMerchant(n, ar):            # this tasks can be done differently with itterators and tuples
+                                    # I am doing here a bit harder to track array version
     p = 0
-    while ar:
-        v = ar[0]
+    while ar:                       # while still there is something in array
+        v = ar[0]                   # we declare v to first item in array
         try:
-            # ar = []
-            ar.pop(ar.index(v))
-            ar.pop(ar.index(v))
-            p += 1
+            ar.pop(ar.index(v))     # we pop up the first value of v which is current first
+            ar.pop(ar.index(v))     # if there is another one value like that its a matching par and we poped it out
+            p += 1                  # if removing element didnt raise an exception we increment the p
         except (IndexError, ValueError):
-            continue
+            continue                # we just keep going here in a loop as long as there is something in array
 
-    return p
+    return p                        # finally we return value
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
